5d9859b6ef3bc2e7029e1762589e9b95e949f3a1
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 14:34:39 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,3 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
-
 def select_winner(passengers)
-  # add the code snippet here!
+  return passengers[:suite_a] if passengers[:suite_a].start_with?('A')
 end
