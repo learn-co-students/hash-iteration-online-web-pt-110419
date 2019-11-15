b72238ce8977757efd4d32e255c679d2cda6fb68
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-15 01:25:56 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+passengers.each do |suite, name| #We chose #each instead of collect because we want  just the name of the winner
+  if suite == :suite_a && name.start_with?("A") #to check if we have the right suite and if the person in that suite has a name that begins with the letter "A".
+    winner = name #If that condition returns true, we've found our winner! We set their name equal to the winner variable and end our iteration.
+  end
+end
+ 
+winner #to return the name of the lucky winner.
 end
