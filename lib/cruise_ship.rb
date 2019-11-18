509561ad5e7bc798a23aca498f60a977862ec848
Dimitri Gedda <gratitudeadore@fastmail.com>
2019-11-18 04:27:37 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each {|key, value|
+    if key == :suite_a && value.start_with?("A")
+      winner = value
+    end
+  }
+  winner
 end
