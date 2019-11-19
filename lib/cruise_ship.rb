c9b94ee87a44be0ddf5b3b11fe64667b77a18c5f
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-19 01:05:29 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,15 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+winner = ""
+
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with? ("A")
+    winner = name
+  end
+end
+
+
 def select_winner(passengers)
   # add the code snippet here!
 end
